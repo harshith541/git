861b9264817b746ed23329158a858bbba18d8b24
Harshith.A <harshithshetty803@gmail.com>
2025-11-23 21:38:22 +0530

    uhtd

--- factorial.java
+++ factorial.java
@@ -3,7 +3,7 @@ public class factorial {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter a number");
-        int num=sc.nextInt();
+        int  num=sc.nextInt();
         int fact=1;
         for(int i = 1; i <= num; i++) {
             fact = fact * i;
@@ -11,7 +11,7 @@ public class factorial {
 
         System.out.println("Factorial of " + num + " is: " + fact);
 
-        sc.close();
+         sc.close();
 
     }    
 }
