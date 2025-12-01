2fd7b4a4e96776523e2b242e161bdea263833849
Harshith.A <harshithshetty803@gmail.com>
2025-12-01 21:34:50 +0530

    birthday

---
+++ BirthdayWish.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class BirthdayWish {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the name: ");
+        String name = sc.nextLine();
+
+        System.out.println(" Happy Birthday " + name + "");
+        System.out.println("Wishing you a wonderful year ahead!");
+    }
+}
