c9afe5f0bd785abe56dd5bea420df0d2044248db
Harshith.A <harshithshetty803@gmail.com>
2026-01-07 21:30:48 +0530

    fdss

--- OddEven.java
+++ OddEven.java
@@ -3,7 +3,7 @@ public class OddEven {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
 
-        System.out.println("Enter A Number:");
+         System.out.println("Enter A Number:");
         int num=sc.nextInt();
         if(num%2==0){
             System.out.println("This is even number");
