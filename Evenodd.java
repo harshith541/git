ad0ded296af3b234d96a423ba114cdd646422ef4
Harshith.A <harshithshetty803@gmail.com>
2026-01-08 21:50:54 +0530

    hvdx

--- Evenodd.java
+++ Evenodd.java
@@ -1,6 +1,6 @@
 class Evenodd{
     public static void main(String[] args){
-        int num=6;
+        int  num=6;
         if(num%2==0){
             System.out.println("Even number");
         }else{
