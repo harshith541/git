c0934f3058c44da7f8ded162d863e5bafe921664
Harshith.A <harshithshetty803@gmail.com>
2026-01-09 21:35:39 +0530

    chethana

--- twonum.java
+++ twonum.java
@@ -1,7 +1,7 @@
 public class twonum {
    public static void main(String[] args){
     int a=10,b=20;
-    if(a>b){
+     if(a>b){
          System.out.println("A is largest");
     }else{
         System.out.println("B is largest");
