0c1f348cbd56c96aa2bd15b1c1e55ef60935a4f6
Harshith.A <harshithshetty803@gmail.com>
2026-01-12 18:51:27 +0530

    jojbgjb

--- Displayfun.java
+++ Displayfun.java
@@ -4,7 +4,7 @@ class Animal{
     String type;
      void Displayfun()
      {
-        System.out.println("Name:"+name);
+         System.out.println("Name:"+name);
         System.out.println("Age:"+age);
         System.out.println("Type:"+type);
      }
