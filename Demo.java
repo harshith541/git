9b92834a8dc4b42aa36119cd187f75903b25aa22
Harshith.A <harshithshetty803@gmail.com>
2026-01-29 22:59:04 +0530

    kkhjhk

--- Demo.java
+++ Demo.java
@@ -3,7 +3,7 @@ class Demo{
     public static void main(String args[]){
          Scanner sc=new Scanner(System.in);
            System.out.println("Enter 1st number:");
-         int num1=sc.nextInt();
+          int num1=sc.nextInt();
            System.out.println("Enter 2nd number:");    
          int num2=sc.nextInt();
            int sum=num1+num2;
