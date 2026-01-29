9cd2ccba25a9cc552823b118af549ac58721d9af
Harshith.A <harshithshetty803@gmail.com>
2026-01-29 22:58:11 +0530

    ghf

--- birthday.java
+++ birthday.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class birthday{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter Your Name:");
+          System.out.println("Enter Your Name:");
         String name =sc.nextLine();
         System.out.println("Happy Birthday" + name + "");
     }
