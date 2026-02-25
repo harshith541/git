2f353f8d8be81e4e4ed975c99c1359cc52488440
Harshith.A <harshithshetty803@gmail.com>
2026-02-25 22:00:01 +0530

    gvf

---
+++ EmployeeForm.java
@@ -0,0 +1,20 @@
+import javax.swing.*; 
+import java.awt.*; 
+import java.awt.event.*; 
+public class EmployeeForm extends JFrame implements ActionListener 
+{ 
+JLabel l1,l2,l3,l4,l5,l6; 
+JTextField tfld,tfname,tfage; 
+JRadioButton rbmale,rbfemale; 
+JComboBox<String>cbdept; 
+JCheckBox cbjava,cbpython,cbcpp,cbSQL; 
+JButton btnsubmit,btnreset; 
+ButtonGroup bg; 
+EmployeeForm() 
+{ 
+setTitle("Employee Registration Form"); 
+setSize(500,500); 
+setLayout(new GridLayout(9,2,5,5)); 
+setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+}
+}
