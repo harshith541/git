e65307fcafd3a4feaee64a3f2f8c1d31eafc60f4
Harshith.A <harshithshetty803@gmail.com>
2026-03-08 19:24:46 +0530

    dsaa

--- HelloApplet.java
+++ HelloApplet.java
@@ -2,7 +2,7 @@ import java.applet.Applet;
 import java.awt.Graphics;
 
 public class HelloApplet extends Applet {
-    public void paint(Graphics g) {
+     public void paint(Graphics g) {
         g.drawString("Hello Applet", 50, 50);
     }
 }
